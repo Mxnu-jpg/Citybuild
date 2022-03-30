03d2a77330ef46418636284cc80790f737e0d7f1
Mxnu <reinprecht.786@gmail.com>
2022-03-31 00:30:26 +0200

    Feierabend

--- src/at/htblakaindorf/AHIF18/UI.java
+++ src/at/htblakaindorf/AHIF18/UI.java
@@ -20,7 +20,7 @@ public class UI {
 
     public UI(GamePanel gp){
         this.gp = gp;
-        amount_of_items_in_Bottom_UI = 4;
+        amount_of_items_in_Bottom_UI = 8;
         height_of_Bottom_UI = gp.getScreenHeight()/6;
         height_of_Top_UI = gp.getScreenHeight()/16;
         menuetilesize = height_of_Bottom_UI/2;
@@ -35,13 +35,13 @@ public class UI {
                     JOptionPane.ERROR_MESSAGE);
         }
     }
-    private int calculatemenuepos(int i){//Synchron zu amount_of_items_in_UI
+    private int calculatemenuepos(int i){//Synchron zu amount_of_items_in_UI-1
         //erstes Und letzes Element gleicher Abstand
         if(i == 0)
             return (gp.getScreenWidth()/amount_of_items_in_Bottom_UI)/2;
         if(i == amount_of_items_in_Bottom_UI-1)
             return gp.getScreenWidth() - (gp.getScreenWidth()/amount_of_items_in_Bottom_UI)/2 - menuetilesize;
-        return (((gp.getScreenWidth() - menuetilesize - menuetilesize/2)/amount_of_items_in_Bottom_UI)*i) + calculatemenuepos(0); // letzter Wert zu hoch != jeder andere
+        return (((gp.getScreenWidth() - ((gp.getScreenWidth()/amount_of_items_in_Bottom_UI)/2 - menuetilesize/2))/amount_of_items_in_Bottom_UI)*i) + calculatemenuepos(0); // letzter Wert zu hoch != jeder andere
     }
 
     private void setUIimages(int index, String imagePath) throws IOException {
@@ -59,6 +59,10 @@ public class UI {
         g2.drawImage(tile[10].image, calculatemenuepos(1),   gp.getScreenHeight() - (gp.getScreenHeight()/8), null);
         g2.drawImage(tile[10].image, calculatemenuepos(2),   gp.getScreenHeight() - (gp.getScreenHeight()/8), null);
         g2.drawImage(tile[10].image, calculatemenuepos(3),   gp.getScreenHeight() - (gp.getScreenHeight()/8), null);
+        g2.drawImage(tile[10].image, calculatemenuepos(4),   gp.getScreenHeight() - (gp.getScreenHeight()/8), null);
+        g2.drawImage(tile[10].image, calculatemenuepos(5),   gp.getScreenHeight() - (gp.getScreenHeight()/8), null);
+        g2.drawImage(tile[10].image, calculatemenuepos(6),   gp.getScreenHeight() - (gp.getScreenHeight()/8), null);
+        g2.drawImage(tile[10].image, calculatemenuepos(7),   gp.getScreenHeight() - (gp.getScreenHeight()/8), null);
     }
     public void createTopMenu(Graphics2D g2, int x, int y, int width, int height){
         g2.setColor(Color.GRAY);
