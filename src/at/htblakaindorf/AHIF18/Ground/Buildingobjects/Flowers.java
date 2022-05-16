19faf0a74f6a08f11e71bbdb654c046e044f05e9
MarcelSchmidl <smimaa18@htl-kaindorf.at>
2022-05-16 11:27:33 +0200

    moin

---
+++ src/at/htblakaindorf/AHIF18/Ground/Buildingobjects/Flowers.java
@@ -0,0 +1,12 @@
+package at.htblakaindorf.AHIF18.Ground.Buildingobjects;
+
+import at.htblakaindorf.AHIF18.Ground.Tile;
+
+public class Flowers extends Tile {
+    int col, row;
+
+    public Flowers(int col, int row) {
+        this.col = col;
+        this.row = row;
+    }
+}
