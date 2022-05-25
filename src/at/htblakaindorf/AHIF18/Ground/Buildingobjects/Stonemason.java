be2949be5db14410e1c2852dfebf2af139fc6670
Mxnu <reinprecht.786@gmail.com>
2022-05-25 21:41:10 +0200

    Water

---
+++ src/at/htblakaindorf/AHIF18/Ground/Buildingobjects/Stonemason.java
@@ -0,0 +1,9 @@
+package at.htblakaindorf.AHIF18.Ground.Buildingobjects;
+
+public class Stonemason extends Building {
+
+    public Stonemason(String name, boolean collision, int id, boolean building, int[] costs, int[] earnings, int col, int row) {
+        super(name, collision, id, building, costs, earnings, col, row);
+    }
+}
+
